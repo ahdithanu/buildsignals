e6f459852e3b937538cf5d302a06fd17f5e6f154
ahdithebomb <ahdithan.uthayakumar@sjsu.edu>
2026-08-23 13:31:28 -0500

    Fix San Jose rollout expectations

--- tests/test_ingestion_health.py
+++ tests/test_ingestion_health.py
@@ -918,6 +918,8 @@ def test_ingestion_candidates_endpoint_returns_structured_queue(client):
         "tulsa_ok_development_plans",
         "charleston_wv_energov_permits",
         "cheyenne_wy_opengov_permits",
+        "san_jose_ca_planning_director_hearings",
+        "san_jose_ca_large_energy_projects",
     }
     assert all(
         row["catalog_backed"]
